a38e6956a07afe486d63358238957c50bb480798
onslaught7 <krutibashmohapatra7@gmail.com>
2025-11-20 16:05:21 +0000

    add the search_movies

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse # To parse command-line arguments
+from typing import List
+import json
+import os
+
+def search_movies(query: str) -> List[str]:
+    try:    
+        with open("./data/movies.json", "r") as f:
+            movies_dict = json.load(f)
+            
+    except FileNotFoundError:
+        print("Error: ./data/movies.json not found.")
+        return []
+
+    search_results = []
+
+    for movie in movies_dict["movies"]:
+        if query.lower() in movie["title"].lower():
+            result = (movie["id"], movie["title"])
+            search_results.append(result)
+
+    # Order by ascending order of IDs and truncate to top 5 results
+    search_results.sort(key=lambda x: x[0])
+    top_results = search_results[:5]
+
+    return [title for _, title in top_results]
 
 
 def main() -> None:
@@ -16,6 +41,13 @@ def main() -> None:
         case "search":
             # print the search query here
             print(f"Searching for: {args.query}")
+            results = search_movies(args.query)
+
+            if results:
+                for i, result in enumerate(results, start=1):
+                    print(f"{i}. {result}")
+            else:
+                print("No results found.")
         case _:
             parser.print_help()
 
