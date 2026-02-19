dbf77da6ec7b4e1863799a00f1c3443e4bed8d66
onslaught7 <krutibashmohapatra7@gmail.com>
2026-02-19 10:08:10 +0000

    add normalize

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,13 +1,25 @@
 import argparse
+from lib.hybrid_search import (
+    HybridSearch,
+    min_max_normalization
+)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
-    parser.add_subparsers(dest="command", help="Available commands")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("normalize", help="Accepts a list of scores and prints the normalized scores")
+    search_parser.add_argument("scores", nargs="+", type=float)
+    
+
 
     args = parser.parse_args()
 
     match args.command:
+        case "normalize":
+            min_max_normalization(args.scores)                
         case _:
             parser.print_help()
 
--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -1,7 +1,30 @@
 import os
 
-from semantic_search import ChunkedSemanticSearch
+# local import from the same package
+from .semantic_search import ChunkedSemanticSearch
 from src.inverted_index import InvertedIndex
+
+
+def min_max_normalization(scores: list[float]) -> None:
+    if len(scores) == 0:
+        return
+
+    min_score = min(scores)
+    max_score = max(scores)
+
+    if max_score == min_score:
+        for _ in range(len(scores)):
+            print(f"* {1.0}")
+        return
+
+    normalized_list = []
+
+    for score in scores:
+        normalized_score = (score - min_score) / (max_score - min_score)
+        normalized_list.append(normalized_score)
+
+    for score in normalized_list:
+        print(f"* {score:.4f}")
 
 
 class HybridSearch:
