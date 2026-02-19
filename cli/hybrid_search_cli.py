216fcd0b54e573268ecd2f9012a53b36ce2e2acd
onslaught7 <krutibashmohapatra7@gmail.com>
2026-02-19 07:45:00 +0000

    fix command error in the hybrid_search_cli.py

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -2,15 +2,15 @@ import argparse
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Hybrid Search Cli")
+    parser = argparse.ArgumentParser(description="Hybrid Search CLI")
     parser.add_subparsers(dest="command", help="Available commands")
 
     args = parser.parse_args()
 
-    match args.command():
+    match args.command:
         case _:
             parser.print_help()
 
-    
+
 if __name__ == "__main__":
     main()
